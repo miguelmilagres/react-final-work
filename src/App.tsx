26c5a11d113a17d560e66566d9a4e5bd3634475b
miguelmilagres <miguelmdemacedo@gmail.com>
2024-05-21 11:09:08 -0300

    projeto sem bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,15 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 
-import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
 import Home from "./assets/pages/Home";
 import Login from "./assets/pages/Login";
 import Details from "./assets/pages/ProductDetails";
 import PageNotFound from "./assets/pages/PageNotFound";
 import Cart from "./assets/pages/Cart";
-import MainNavbar from "./assets/components/elements/NavBar";
 
 function App() {
     return (
         <>
-            <MainNavbar />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route
